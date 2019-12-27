f9fd761f5841aa53f248b5752591a0339a8004d5
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2019-12-27 16:30:09 +0100

    Configure using hazelcast.xml

--- src/main/java/guides/hazelcast/quarkus/HazelcastClientConfig.java
+++ src/main/java/guides/hazelcast/quarkus/HazelcastClientConfig.java
@@ -1,6 +1,7 @@
 package guides.hazelcast.quarkus;
 
 import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientClasspathXmlConfig;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
 
@@ -12,10 +13,6 @@ public class HazelcastClientConfig {
 
     @Produces
     HazelcastInstance createInstance() {
-        ClientConfig clientConfig = new ClientConfig();
-        String[] members = System.getenv("HAZELCAST_IP").split(",");
-
-        clientConfig.getNetworkConfig().addAddress(members);
-        return HazelcastClient.newHazelcastClient(clientConfig);
+        return HazelcastClient.newHazelcastClient(new ClientClasspathXmlConfig("hazelcast.xml"));
     }
 }
